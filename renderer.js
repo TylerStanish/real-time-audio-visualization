2651765004d520c5ab5f0fb90dc7b6065d8d1559
Tyler Stanish <tystanish@gmail.com>
2018-09-08 12:29:09 -0500

    well it seems like its not my fault this isn't working. everything seems to be fine

--- renderer.js
+++ renderer.js
@@ -8,17 +8,17 @@ const coreAudio = require('node-core-audio');
 
 
 function render(arr=new Array(16)){
-  let content = document.getElementById('content')
-
+  // let content = document.getElementById('content');
   const width = window.outerWidth;
   for(let i=0; i<16; i++){
-    let div = document.createElement('span');
-    div.className = 'bar';
+    // let div = document.createElement('span');
+    let div = document.getElementById('bar' + i);
+    // div.className = 'bar';
     const barPos = i*(width/16);
     div.style.left = barPos + 'px';
     div.style.width = width/16 + 'px';
-    div.style.height = arr[i]*1000 + 'px';
-    content.appendChild(div);
+    div.style.height = arr[i]*1e13 + 'px';
+    // content.appendChild(div);
   }
 }
 
@@ -32,39 +32,44 @@ function runningAverage(oldVal=0, len, newVal){
 let count = 1;
 function processAudio( inputBuffer ) {
 
-  // if(count % 24 !== 0){
-  //   count++;
-  //   return inputBuffer;
-  // }else{
-  //   debugger;
-    // count = 1;
-  // }
-
   let bars = new Array(16);
   for(let i=0; i<inputBuffer[0].length; i++){
     const index = Math.floor(i/16);
     const distanceBetweenStart = i - index*16;
     bars[index] = runningAverage(bars[index], distanceBetweenStart, inputBuffer[0][i]);
-    // render(bars);
+    
+    // count += 1;
+    // if(count % 64 === 0){
+    //   console.log(inputBuffer);
+    //   console.log(bars);
+    //   count = 1;
+    // }
+    // inputBuffer[0][i] = 0;
+    setTimeout(() => render(bars), 0);
   }
 
   return inputBuffer;
 }
 
 
-let engine = coreAudio.createNewAudioEngine();
-// engine.setOptions({sampleRate: 5000});
-// engine.addAudioCallback( processAudio );
 
-setInterval(() => {
-  // Grab a buffer
-  let buffer = engine.read();
-  console.log(buffer);
-// Silence the 0th channel
-  for( let iSample=0; iSample<buffer[0].length; ++iSample ){
-    buffer[0][iSample] = 0.0;
-  }
-  console.log(buffer);
-// Send the buffer back to the sound card
-  engine.write(buffer);
-}, 1000);
+
+// var coreAudio = require("node-core-audio");
+let engine = coreAudio.createNewAudioEngine();
+
+var options = {
+    inputChannels: 1,
+    outputChannels: 1,
+    useMicrophone: false,
+    // inputDevice: 1,
+    // outputDevice: 0
+};
+
+engine.setOptions(options);
+console.log(engine.getOptions());
+console.log(engine.getDeviceName(0), engine.getDeviceName(1));
+engine.addAudioCallback(function (inputBuffer) {
+  let newBuffer = inputBuffer.slice();
+  console.log(inputBuffer);
+  return processAudio(newBuffer);
+});
