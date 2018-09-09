a16c352e2d1191fd867b7c8c23f8c6423fc2df40
Tyler Stanish <tystanish@gmail.com>
2018-09-08 19:08:15 -0500

    added variable number of bars

--- renderer.js
+++ renderer.js
@@ -1,94 +1,87 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const coreAudio = require('node-core-audio')
 
-const coreAudio = require('node-core-audio');
+const WIDTH = 512
 
-
-
-function render(arr=new Array(16)){
-  // let content = document.getElementById('content');
-  const width = window.outerWidth;
-  for(let i=0; i<16; i++){
-    // let div = document.createElement('span');
-    let div = document.getElementById('bar' + i);
-    // div.className = 'bar';
-    const barPos = i*(width/16);
-    div.style.left = barPos + 'px';
-    div.style.width = width/16 + 'px';
-    div.style.height = arr[i]*2000000 + 'px';
-    // content.appendChild(div);
+function render(arr = new Array(WIDTH)){
+  const width = window.outerWidth
+  for(let i = 0; i < WIDTH; i++){
+    let div = document.getElementById('bar' + i)
+    const barPos = i * (width / WIDTH)
+    div.style.left = barPos + 'px'
+    div.style.width = width / WIDTH + 'px'
+    div.style.height = Math.log(i)*arr[i] * 200000 + 'px'
   }
 }
 
 
-
-function runningAverage(oldVal=0, len, newVal){
-  // debugger;
-  return (oldVal+newVal)/(len+1);
+function runningAverage(oldVal = 0, len, newVal){
+  return (oldVal + newVal) / (len + 1)
 }
 
-let count = 1;
-function processAudio( inputBuffer ) {
+let count = 1
 
-  let bars = new Array(16);
-  for(let i=0; i<inputBuffer.length; i++){
-    const index = Math.floor(i/16);
-    const distanceBetweenStart = i - index*16;
-    bars[index] = runningAverage(bars[index], distanceBetweenStart, inputBuffer[i]);
-    
-    // count += 1;
-    // if(count % 64 === 0){
-    //   console.log(inputBuffer);
-    //   console.log(bars);
-    //   count = 1;
-    // }
-    // inputBuffer[0][i] = 0;
-    setTimeout(() => render(bars), 0);
-  }
-
+function processAudio(inputBuffer){
+  if(count % 2 === 0){
+    // debugger;
+    setTimeout(() => render(inputBuffer), 0)
+    count = 1;
+  }else{ count += 1; }
   return inputBuffer;
+
+  let bars = new Array(WIDTH)
+  for(let i = 0; i < inputBuffer.length; i++){
+    const index = Math.floor(i / WIDTH)
+    const distanceBetweenStart = i - index * WIDTH
+    bars[index] = runningAverage(bars[index], distanceBetweenStart, inputBuffer[i])
+  }
+  if(count % 2 === 0){
+    // debugger;
+    setTimeout(() => render(bars), 0)
+    count = 1;
+  }else{ count += 1; }
+
+  return inputBuffer
 }
 
+let engine = coreAudio.createNewAudioEngine()
 
+const options = {
+  inputChannels: 1,
+  outputChannels: 1,
+  inputDevice: 2
+}
 
-
-// var coreAudio = require("node-core-audio");
-let engine = coreAudio.createNewAudioEngine();
-
-var options = {
-    inputChannels: 1,
-    outputChannels: 1,
-    inputDevice: 2,
-    // outputDevice: 4
-    // inputDevice: 1,
-    // outputDevice: 0
-};
-
-engine.setOptions(options);
-console.log(engine.getOptions());
+engine.setOptions(options)
+console.log(engine.getOptions())
 console.log(
-  engine.getDeviceName(0), 
-  engine.getDeviceName(1), 
-  engine.getDeviceName(2), 
-  engine.getDeviceName(3), 
+  engine.getDeviceName(0),
+  engine.getDeviceName(1),
+  engine.getDeviceName(2),
+  engine.getDeviceName(3),
   engine.getDeviceName(4)
-);
+)
 
 
-const ft = require('fourier-transform');
-const db = require('decibels');
+const ft = require('fourier-transform/asm')
+const db = require('decibels')
 
-const frequency = 440;
-const size = 1024;
-const sampleRate = 44100;
+function start(){
+  const content = document.getElementById('content')
+  for(let i = 0; i < WIDTH; i++){
+    let span = document.createElement('span')
+    span.className = 'bar'
+    span.id = 'bar' + i
+    content.appendChild(span)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', start)
 
 
-engine.addAudioCallback(function (inputBuffer) {
-  let spectrum = ft(inputBuffer[0]);
+engine.addAudioCallback(function(inputBuffer){
+  let spectrum = ft(inputBuffer[0])
   let decibels = spectrum.map((value) => db.fromGain(value))
-  console.log(spectrum);
-  console.log(decibels);
-  processAudio(spectrum);
+  // console.log(spectrum)
+  processAudio(spectrum)
   // return inputBuffer;
-});
+})
